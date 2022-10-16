a0143008c230666d8ce5a5ca0eb805958f376ec9
liuhang <liuhang@mail.nwpu.edu.cn>
2022-10-16 20:14:13 +0800

    迷宫问题

---
+++ homework/Experiment/1142-balloon.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+bool Big_win=false;
+
+
+bool IsPrime(int a){
+    for(int i=2;i<a;i++){
+        if(a%2)
+            return false;
+    }
+    return true;
+}
+int main(){
+    while(1){
+        int a,b;
+        if(IsPrime(a)||)
+    }
+}
